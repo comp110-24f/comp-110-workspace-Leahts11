b597a7d041de2572afd5b60bca3eeff8e1359fb8
leahts <leahts@unc.edu>
2024-11-06 15:20:09 -0500

    pushing ex07

--- exercises/ex07/river.py
+++ exercises/ex07/river.py
@@ -1,15 +1,18 @@
 """File to define River class."""
 
+__author__ = "730648923"
+
 from ex07.fish import Fish
 from ex07.bear import Bear
 
+
 class River:
-    
+
     day: int
     fish: list[Fish]
     bears: list[Bear]
-    
-    def __init__(self, num_fish: int, num_bears:int):
+
+    def __init__(self, num_fish: int, num_bears: int):
         """New River with num_fish Fish and num_bears Bears"""
         self.day: int = 0
         self.fish: list[Fish] = []
@@ -21,23 +24,72 @@ class River:
             self.bears.append(Bear())
 
     def check_ages(self):
+        """making sure ages align with given ages that survive"""
+        surviving_bears = []
+        surviving_fish = []
+
+        for fish in self.fish:
+            if fish.age <= 3:
+                surviving_fish.append(fish)
+
+        for bear in self.bears:
+            if bear.age <= 5:
+                surviving_bears.append(bear)
+
+        self.fish = surviving_fish
+        self.bears = surviving_bears
         return None
 
-    def bears_eating(self):
-        return None
-    
     def check_hunger(self):
+        """removes bears with hunger score < 0"""
+        surviving_bears = []
+        for bear in self.bears:
+            if bear.hunger_score >= 0:
+                surviving_bears.append(bear)
         return None
-        
+
+    def remove_fish(self, num: int):
+        """Remove given amount of Fish from the River starting from  front"""
+        for _ in range(num):
+            if self.fish:
+                self.fish.pop(0)
+
+    def bears_eating(self):
+        """each Bear eats fish from the river if enough are available."""
+        for bear in self.bears:
+            if len(self.fish) >= 5:
+                self.remove_fish(3)
+                bear.eat(3)
+
+        return None
+
     def repopulate_fish(self):
+        """Add new fish to  river based on the current fish population"""
+        num_fish = len(self.fish)
+        new_fish = (num_fish // 2) * 4
+
+        while new_fish > 0:
+            self.fish.append(Fish())
+            new_fish -= 1
         return None
-    
+
     def repopulate_bears(self):
+        """Add new bears to river based on the current bear population"""
+        num_bears = len(self.bears)
+        new_bears = num_bears // 2
+
+        while new_bears > 0:
+            self.bears.append(Bear())
+            new_bears -= 1
         return None
-    
+
     def view_river(self):
+        """Print day and population of bears and fish"""
+        print(f"~~~ Day{self.day}: ~~~")
+        print(f"Fish population:{len(self.fish)}")
+        print(f"Bear population:{len(self.bears)}")
         return None
-            
+
     def one_river_day(self):
         """Simulate one day of life in the river"""
         # Increase day by 1
@@ -60,4 +112,14 @@ class River:
         self.repopulate_bears()
         # Visualize River
         self.view_river()
-            
+
+    def one_river_week(self):
+        # simulate 7 days of life in river
+        self.one_river_day()
+        self.one_river_day()
+        self.one_river_day()
+        self.one_river_day()
+        self.one_river_day()
+        self.one_river_day()
+        self.one_river_day()
+        return None
---
+++ exercises/ex07/river_simulation.py
@@ -0,0 +1,11 @@
+"""Simulating river class"""
+
+__author__ = "730648923"
+
+from ex07.river import River
+
+my_river = River(10, 2)
+
+my_river.view_river()
+
+my_river.one_river_week()
